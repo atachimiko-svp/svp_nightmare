b4267bbcda0a5ef68475ae13c5fe1278eccf6987
atachimiko <atachi.miko@gmail.com>
2016-11-20 10:36:18 +0900

    サーバ同期のサービス追加

---
+++ source/SVP.Service.Nadesico/.NET4.5/Nadesico.Service/ApplicationInterfaceService.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nadesico.Service
+{
+	[ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Single, InstanceContextMode = InstanceContextMode.Single, IncludeExceptionDetailInFaults = true)]
+	public class ApplicationInterfaceService : IApplicationInterfaceService
+	{
+
+
+		#region フィールド
+
+		static IMapper Mapper;
+		static MapperConfiguration MapperConfig;
+
+		#endregion フィールド
+
+
+		#region コンストラクタ
+
+		public ApplicationInterfaceService()
+		{
+			// マッピングするクラスの紐付け設定
+			MapperConfig = new MapperConfiguration(cfg =>
+			{
+				//cfg.CreateMap<Content, SVP.CIL.Domain.Conrent>();
+			});
+
+			Mapper = MapperConfig.CreateMapper();
+		}
+
+		#endregion コンストラクタ
+
+
+		#region メソッド
+
+		public void Login()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Logout()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion メソッド
+
+	}
+}
---
+++ source/SVP.Service.Nadesico/.NET4.5/Nadesico.Service/IApplicationInterfaceService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nadesico.Service
+{
+	/// <summary>
+	/// アプリケーション通信サービス
+	/// </summary>
+	[ServiceContract(SessionMode = SessionMode.Required)]
+	public interface IApplicationInterfaceService
+	{
+
+
+		#region メソッド
+
+		[OperationContract(IsInitiating = true, IsTerminating = false)]
+		void Login();
+
+		[OperationContract(IsInitiating = false, IsTerminating = true)]
+		void Logout();
+
+		#endregion メソッド
+	}
+}
